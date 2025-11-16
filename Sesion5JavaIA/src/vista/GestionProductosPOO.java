7a6264314184ff8ddb1cf6db48eec8779f8919e9
Bryan-2298 <is.bryan22@gmail.com>
2025-11-15 18:13:21 -0600

    Version 2.0 Cambio Amador

--- Sesion5JavaIA/src/vista/GestionProductosPOO.java
+++ Sesion5JavaIA/src/vista/GestionProductosPOO.java
@@ -15,34 +15,76 @@ public class GestionProductosPOO
         //Para uso y llamado de los metodos
         GestorProductos gestor = new GestorProductos();
         
-        //Para alta de productos
-        System.out.println("Sistema de productos\nCaptura ID: ");
-        int id = entradaDato.nextInt();
+        int ciclo;
         
-        System.out.println("Captura Nombre: ");
-        String nombre = entradaDato.next();
+        do
+        {//Inicia do
+            System.out.println("Menu Productos\n1.- Agregar.\n2.- Consultar.\n3.- Eliminar.\n4.- Salir.");
+            ciclo = entradaDato.nextInt();
+            
+            switch (ciclo) 
+            {//Inicia switch
+                
+                case 1: 
+                    
+                    //Para alta de productos
+                    System.out.println("Sistema de productos\nCaptura ID: ");
+                    int id = entradaDato.nextInt();
         
-        System.out.println("Captura Precio: ");
-        double precio = entradaDato.nextDouble();
+                    System.out.println("Captura Nombre: ");
+                    String nombre = entradaDato.next();
         
-        System.out.println("Captura Stock: ");
-        int stock = entradaDato.nextInt();
+                    System.out.println("Captura Precio: ");
+                    double precio = entradaDato.nextDouble();
         
-        //Almacena N datos
-        gestor.agregarProducto(new Producto(id, nombre, precio, stock));
+                    System.out.println("Captura Stock: ");
+                    int stock = entradaDato.nextInt();
         
-        //Consultar productos
-        gestor.listarProductos();
+                    //Almacena N datos
+                    gestor.agregarProducto(new Producto(id, nombre, precio, stock));
+              
+                    break;
+                    
+                case 2: 
+                    
+                    //Consultar productos
+                    gestor.listarProductos();
+                    
+                    break;
+                case 3: 
+                    
+                    //Buscar por id
+                    System.out.println("ID a buscar: ");
+                    Producto p = gestor.buscarId(entradaDato.nextInt());
+                    System.out.println(p != null ? p : "Producto no encontrado.");
         
-        //Buscar por id
-        System.out.println("ID a buscar: ");
-        Producto p = gestor.buscarId(entradaDato.nextInt());
-        System.out.println(p != null ? p : "Producto no encontrado.");
+                    //Eliminar producto
+                    System.out.println("Id a eliminar: ");
+                    gestor.eliminarProducto(entradaDato.nextInt());
         
-        //Eliminar producto
-        System.out.println("Id a eliminar: ");
-        gestor.eliminarProducto(entradaDato.nextInt());
+                    //Consultar par actualizar
+                    gestor.listarProductos();
+                    
+                    break;
+                case 4: 
+                    
+                    System.out.println("Saliendo del sistema...");
+                    
+                    break;
+                default: 
+                    
+                    System.out.println("Error! Solo tengo opcion 1, 2, 3 y 4...");
+                    
+                    break;
+            
+            }//Termina switch
+            
+        }while(ciclo != 4);//Termina do
+                
         
+        
+
+
     }//Termina main
     
     
